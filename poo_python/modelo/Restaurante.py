fcc370b29bc0c62faae671ba30903747a7aa0016
Lucas_v.Gonca <lucxas8@hotmail.com>
2026-06-03 12:23:00 -0300

    Update Restaurante.py

--- poo_python/modelo/Restaurante.py
+++ poo_python/modelo/Restaurante.py
@@ -1,32 +1,40 @@
 class Restaurante:
-    Restaurantes = []
+    restaurantes = []
 
     def __init__(self,nome,categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
-        Restaurante.Restaurantes.append(self)
+        self.nome = nome.title()
+        self.categoria = categoria.title()
+        self._ativo = False # ._ torna ele privado como private em typescrit
+        Restaurante.restaurantes.append(self)
 
     def mudar_estado(self):
-        self.ativo = not self.ativo
-        print("o Restaurante foi ativado") if self.ativo else print("O Restaurante foi desativado")
+        self._ativo = not self._ativo
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria} | {"Ativado" if self.ativo else "Desativado"}'
+        return f'{self.nome.ljust(25)} | {self.categoria.ljust(25)} | {self.ativo}'
     
     '''ou fazer por uma função convencional sem os metodos de python
 
         def mostrar_Restaurante(self):
-            ativo = 'Ativado' if self.ativo == True else 'Desativado'
+            ativo = 'Ativado' if self._ativo == True else 'Desativado'
             print(f"o Restaurante {self.nome} com a categoria sendo de {self.categoria} está {ativo}") 
     
     '''
-    def listar_Restaurantes():
-        for restaurante in Restaurante.Restaurantes:
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f"{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}")
+        for restaurante in cls.restaurantes:
             print(restaurante)
+    
+    @property
+    def ativo(self):
+        return '☑ ativo' if self._ativo else '☒ desativo'
+    
 
+    
+restaurante_praca = Restaurante('Ferrari','Italiano')
+restaurante_pizza = Restaurante('Mercedes','Comida alemã')
 
-Restaurante_praca = Restaurante('Ferrari','Italiano')
-Restaurante_pizza = Restaurante('Mercedes','Comida alemã')
+restaurante_pizza.mudar_estado()
 
-Restaurante.listar_Restaurantes()
+Restaurante.listar_restaurantes()
